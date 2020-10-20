591f0ab9ea822af6977286429c634aafb3d95770
Richard Moore <richard.h.moore.jr@gmail.com>
2020-10-20 15:35:23 -0700

    Update code to model basic correct usage of Web API

---
+++ Controllers/InventoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AutoLotDAL.Repos;
+using AutoLotDAL.Models;
+using AutoMapper;
+using System.Reflection.Emit;
+
+namespace CarLotWebAPI.Controllers
+{
+    [RoutePrefix("api/Inventory")]
+    public class InventoryController : ApiController
+    {
+        [HttpGet, Route("")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet, Route("{id}")]
+        public IHttpActionResult Get(int id)
+        {
+            return Ok(id.ToString());
+        }
+    }
+}
